49bc56f23d119efd66ee1ad013e863a17705d8ba
JzBreno <jose.breno.sousa61@aluno.ifce.edu.br>
2024-10-15 10:38:46 -0300

    deletando e inserindo apartir do spring nossas entidades no mongodb

--- src/main/java/com/example/demo/cfg/instantiation.java
+++ src/main/java/com/example/demo/cfg/instantiation.java
@@ -6,7 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
+import com.example.demo.domain.Component;
 import com.example.demo.domain.User;
+import com.example.demo.repository.ComponentsRepository;
 import com.example.demo.repository.UserRepository;
 
 @Configuration
@@ -14,16 +16,24 @@ public class instantiation implements CommandLineRunner{
 	
 	@Autowired
 	UserRepository userRepository;
-	
+	@Autowired
+	ComponentsRepository componentRepository;
 	@Override
 	public void run(String... args) throws Exception {
 		// TODO Auto-generated method stub
 		userRepository.deleteAll();
+		componentRepository.deleteAll();
+		
 		User maria = new User(null, "maria brown", "maria@gmail.com");
 		User joe = new User(null, "joe brown", "joe@gmail.com");
+		Component motherBoard = new Component(null, "mother board Asus","placa mae de alta qaulidade ddr4");
+		
 		try {
 	        userRepository.saveAll(Arrays.asList(maria, joe));
+	        componentRepository.saveAll(Arrays.asList(motherBoard));
 	        System.out.println("Usuários criados com sucesso!");
+	        System.out.println("Componentes criados com sucesso!");
+
 	    } catch (Exception e) {
 	        System.err.println("Erro ao criar usuários: " + e.getMessage());
 	    }
